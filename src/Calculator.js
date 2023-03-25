433d3f948e4573b6e76c217bfeec3704a33503b8
TTRAN70 <107781508+TTRAN70@users.noreply.github.com>
2023-03-25 19:16:48 -0400

    3 files modified

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
 
 export default function Calculator() {
@@ -15,6 +15,12 @@ export default function Calculator() {
   const numberClick = (event) => {
     event.preventDefault()
     if (event.target.value == 0) {
+      if (equal) {
+        setTop(prev => "");
+        setCurrent(prev => "");
+        setEqual(false);
+        setZero(false);
+      }
       if (currentDisplay == "0" && spamZero) {
         setTop(prev => prev + event.target.value);
         setZero(false);
@@ -30,6 +36,11 @@ export default function Calculator() {
       }
     }
     else {
+        if (equal) {
+          setTop(prev => "");
+          setCurrent(prev => "");
+          setEqual(false);
+        }
         if (currentDisplay == "0" && spamZero) {
           setCurrent(prev => "");
           setTop(prev => "");
@@ -55,9 +66,15 @@ export default function Calculator() {
     setCalc(true);
     setZero(true);
     setNeg(true);
+    setEqual(false);
   }
 
   const decimate = () => {
+    if (equal) {
+      setTop(prev => "");
+      setCurrent(prev => "");
+      setEqual(false);
+    }
     if (toggle && currentDisplay == "0" && topDisplay == "") {
       setTop(prev => prev + "0");
     }
@@ -78,8 +95,14 @@ export default function Calculator() {
     event.preventDefault()
     if (!(topDisplay == "" && currentDisplay == "0")) {
       if(calcToggle) {
+        if(equal) {
+          setTop(prev => currentDisplay.substring(1, currentDisplay.length) + event.target.value);
+          setEqual(false);
+        }
+        else {
+          setTop(prev => prev + event.target.value);
+        }
         setCurrent(event.target.value);
-        setTop(prev => prev + event.target.value);
         setCalc(false);
         setToggle(true);
         setZero(false);
@@ -124,33 +147,40 @@ export default function Calculator() {
     if (!(topDisplay == "") && !(topDisplay == "-") && !(isNaN(topDisplay.substring(topDisplay.length-1, topDisplay.length)))) {
       const format = topDisplay.replaceAll("X", "*");
       const calc = eval(format);
-      setCurrent(prev => "= " + calc);
+      if(Number.isInteger(calc)) {
+        setCurrent(prev => "= " + calc);
+      }
+      else {
+        setCurrent(prev => "= " + calc.toFixed(5));
+      }
       setEqual(true);
     }
   }
 
   return (
     <div className="wrapper">
+      <div className="displayWrapper">
       <div className="top">{topDisplay}</div>
       <div className="bottom">{currentDisplay}</div>
+      </div>
       <div className="buttonContainer">
-        <button onClick={() => clear()} className="ac" id="clear" value="AC">AC</button>
-        <button onClick={e => calculate(e)} className="box" id="divide" value="/">/</button>
-        <button onClick={e => numberClick(e)} className="box" id="seven" value="7">7</button>
-        <button onClick={e => numberClick(e)} className="box" id="eight" value="8">8</button>
-        <button onClick={e => numberClick(e)} className="box" id="nine" value="9">9</button>
-        <button onClick={e => calculate(e)} className="box" id="multiply" value="X">X</button>
-        <button onClick={e => numberClick(e)} className="box" id="four" value="4">4</button>
-        <button onClick={e => numberClick(e)} className="box" id="five" value="5">5</button>
-        <button onClick={e => numberClick(e)} className="box" id="six" value="6">6</button>
-        <button onClick={e => calculate(e)} className="box" id="subtract" value="-">-</button>
-        <button onClick={e => numberClick(e)} className="box" id="one" value="1">1</button>
-        <button onClick={e => numberClick(e)} className="box" id="two" value="2">2</button>
-        <button onClick={e => numberClick(e)} className="box" id="three" value="3">3</button>
-        <button onClick={e => calculate(e)} className="box" id="add" value="+">+</button>
-        <button onClick={e => numberClick(e)} className="zero" id="zero" value="0">0</button>
-        <button onClick={() => decimate()} className="box" id="decimal" value=".">.</button>
-        <button onClick={() => evaluate()} className="box" id="equals" value="=">=</button>
+        <button onClick={() => clear()} className="ac press" id="clear" value="AC">AC</button>
+        <button onClick={e => calculate(e)} className="box press" id="divide" value="/">/</button>
+        <button onClick={e => numberClick(e)} className="box press" id="seven" value="7">7</button>
+        <button onClick={e => numberClick(e)} className="box press" id="eight" value="8">8</button>
+        <button onClick={e => numberClick(e)} className="box press" id="nine" value="9">9</button>
+        <button onClick={e => calculate(e)} className="box press" id="multiply" value="X">X</button>
+        <button onClick={e => numberClick(e)} className="box press" id="four" value="4">4</button>
+        <button onClick={e => numberClick(e)} className="box press" id="five" value="5">5</button>
+        <button onClick={e => numberClick(e)} className="box press" id="six" value="6">6</button>
+        <button onClick={e => calculate(e)} className="box press" id="subtract" value="-">-</button>
+        <button onClick={e => numberClick(e)} className="box press" id="one" value="1">1</button>
+        <button onClick={e => numberClick(e)} className="box press" id="two" value="2">2</button>
+        <button onClick={e => numberClick(e)} className="box press" id="three" value="3">3</button>
+        <button onClick={e => calculate(e)} className="box press" id="add" value="+">+</button>
+        <button onClick={e => numberClick(e)} className="zero press" id="zero" value="0">0</button>
+        <button onClick={() => decimate()} className="box press" id="decimal" value=".">.</button>
+        <button onClick={() => evaluate()} className="box press" id="equals" value="=">=</button>
       </div>
     </div>
   );
