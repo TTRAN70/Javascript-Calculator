b57005cf901f077c4be44303b777ca460f44f224
TTRAN70 <107781508+TTRAN70@users.noreply.github.com>
2023-03-25 21:21:02 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 import Calculator from "./Calculator.js";
+import ReactFCCtest from 'react-fcctest';
 
 export default function App() {
   return (
     <div className="mainwrap">
+      <ReactFCCtest />
       <Calculator />
     </div>
   );
